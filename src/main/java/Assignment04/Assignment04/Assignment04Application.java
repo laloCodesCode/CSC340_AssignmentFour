404c32b24a9d94312484180af91e9b1db9dc3bec
Eduardo <eduardoherrerabarra@gmail.com>
2025-03-04 23:39:17 -0500

    inital commit

---
+++ src/main/java/Assignment04/Assignment04/Assignment04Application.java
@@ -0,0 +1,19 @@
+package Assignment04.Assignment04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment04Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Assignment04Application.class, args);
+
+
+
+
+
+	}
+
+}
